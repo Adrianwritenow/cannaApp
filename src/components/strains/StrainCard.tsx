7ebfa2e16debcdeafc4fd00af780cdaa4fa91493
Adrian Rodriguez <Adrianwritenow@gmail.com>
2021-11-11 11:37:44 -0500

    CAN-226--Strain detail page (#15)

--- src/components/strains/StrainCard.tsx
+++ src/components/strains/StrainCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import { Strain } from "../../interfaces/strain";
@@ -11,43 +12,54 @@ export default function StrainCard(data: StrainProps) {
   const { strain } = data;
 
   return (
-    <div
-      key={strain.id}
-      className="relative w-full flex flex-wrap min-w-36"
-      id={`strain-${strain.id}`}
-    >
-      <div className="rounded-lg overflow-hidden relative w-full pb-full">
-
-        <Image
-          src={strain.images[0]}
-          alt={strain.title}
-          layout="fill"
-          objectFit={"cover"}
-          className={"w-full h-full"}
-        />
-      </div>
-      <div className="pt-2 pb-6 text-left text-sm">
-        <h3 className="text-sm font-normal text-gray-700">{strain.title}</h3>
-        <div className="flex flex-col items-start">
-          <p className="sr-only">{strain.rating} out of 5 stars</p>
-          <div className="flex items-center">
-            <span className="font-normal text-gray-500">{strain.rating}</span>
-
-            {[0, 1, 2, 3, 4].map((rating) => (
-              <StarIcon
-                key={rating}
-                className={`    ${
-                  strain.rating > rating ? "text-yellow-400" : "text-gray-200"
-                }
-                  flex-shrink-0 h-4 w-4`}
-                aria-hidden="true"
-              />
-            ))}
-            <p className="font-normal text-gray-500">({strain.reviewCount})</p>
+    <Link href="/strain/1" passHref>
+      <a>
+        <div
+          key={strain.id}
+          className="relative w-full flex flex-wrap min-w-36"
+          id={`strain-${strain.id}`}
+        >
+          <div className="rounded-lg overflow-hidden relative w-full pb-full">
+            <Image
+              src={strain.images[0]}
+              alt={strain.title}
+              layout="fill"
+              objectFit={"cover"}
+              className={"w-full h-full"}
+            />
           </div>
-          <p className="text-sm text-gray-500">{strain.type}</p>
+          <div className="pt-2 pb-6 text-left text-sm">
+            <h3 className="text-sm font-normal text-gray-700">
+              {strain.title}
+            </h3>
+            <div className="flex flex-col items-start">
+              <p className="sr-only">{strain.rating} out of 5 stars</p>
+              <div className="flex items-center">
+                <span className="font-normal text-gray-500">
+                  {strain.rating}
+                </span>
+
+                {[0, 1, 2, 3, 4].map((rating) => (
+                  <StarIcon
+                    key={rating}
+                    className={`    ${
+                      strain.rating > rating
+                        ? "text-yellow-400"
+                        : "text-gray-200"
+                    }
+                  flex-shrink-0 h-4 w-4`}
+                    aria-hidden="true"
+                  />
+                ))}
+                <p className="font-normal text-gray-500">
+                  ({strain.reviewCount})
+                </p>
+              </div>
+              <p className="text-sm text-gray-500">{strain.type}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 }
--- src/components/strains/StrainCardSmall.tsx
+++ src/components/strains/StrainCardSmall.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import { Strain } from "../../interfaces/strain";
@@ -11,39 +12,51 @@ export default function StrainCardSmall(data: StrainProps) {
   const { strain } = data;
 
   return (
-    <div className="w-full flex p-4 items-center">
-      <div className="rounded-lg overflow-hidden w-12 h-12 relative flex-shrink-0 mr-3">
-        <Image
-          src={strain.images[0]}
-          alt={strain.title}
-          layout="fill"
-          objectFit={"cover"}
-        />
-      </div>
-      <div className="text-left text-sm w-full">
-        <div className="flex flex-wrap justify-between">
-          <h3 className="text-sm font-normal text-gray-700">{strain.title}</h3>
-        </div>
-        <div className="flex flex-col items-start">
-          <p className="sr-only">{strain.rating} out of 5 stars</p>
-          <div className="flex items-center">
-            <span className="font-normal text-gray-500">{strain.rating}</span>
-
-            {[0, 1, 2, 3, 4].map((rating) => (
-              <StarIcon
-                key={rating}
-                className={`    ${
-                  strain.rating > rating ? "text-yellow-400" : "text-gray-200"
-                }
-                  flex-shrink-0 h-4 w-4`}
-                aria-hidden="true"
-              />
-            ))}
-            <p className="font-normal text-gray-500">({strain.reviewCount})</p>
+    <Link href="/strain/1" passHref>
+      <a>
+        <div className="w-full flex p-4 items-center">
+          <div className="rounded-lg overflow-hidden w-12 h-12 relative flex-shrink-0 mr-3">
+            <Image
+              src={strain.images[0]}
+              alt={strain.title}
+              layout="fill"
+              objectFit={"cover"}
+            />
           </div>
-          <p className="text-sm text-gray-500">{strain.type}</p>
+          <div className="text-left text-sm w-full">
+            <div className="flex flex-wrap justify-between">
+              <h3 className="text-sm font-normal text-gray-700">
+                {strain.title}
+              </h3>
+            </div>
+            <div className="flex flex-col items-start">
+              <p className="sr-only">{strain.rating} out of 5 stars</p>
+              <div className="flex items-center">
+                <span className="font-normal text-gray-500">
+                  {strain.rating}
+                </span>
+
+                {[0, 1, 2, 3, 4].map((rating) => (
+                  <StarIcon
+                    key={rating}
+                    className={`    ${
+                      strain.rating > rating
+                        ? "text-yellow-400"
+                        : "text-gray-200"
+                    }
+                  flex-shrink-0 h-4 w-4`}
+                    aria-hidden="true"
+                  />
+                ))}
+                <p className="font-normal text-gray-500">
+                  ({strain.reviewCount})
+                </p>
+              </div>
+              <p className="text-sm text-gray-500">{strain.type}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 }

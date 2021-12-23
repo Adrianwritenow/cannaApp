84f1a2d3bebbab507de550a7fda9b000a8f82eb2
Adrianwritenow <adrianwritenow@gmail.com>
2021-12-23 00:49:30 -0500

    fix: commit before merge

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
   experimental: { esmExternals: true },
-  target: "serverless",
+  target: 'serverless',
 
   images: {
-    domains: ["images.unsplash.com", "tailwindui.com", "unsplash.com"],
-    loader: "akamai",
-    path: "",
+    domains: ['images.unsplash.com', 'tailwindui.com', 'unsplash.com'],
+    loader: 'akamai',
+    path: '',
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   api: {
     externalResolver: true,
@@ -19,10 +19,7 @@ module.exports = {
   env: {
     API_URL: process.env.API_URL,
     CLIENT_ID: process.env.CLIENT_ID,
-<<<<<<< HEAD
     SEARCH_URL: process.env.SEARCH_URL,
-=======
     MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
->>>>>>> develop
   },
 };

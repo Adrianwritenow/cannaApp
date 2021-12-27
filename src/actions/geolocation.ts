a2948459d1f55365857391bc1a92ce10527cba90
Tyson Willey <tyson.willey@flhosp.org>
2021-12-27 12:10:19 -0500

    Adds actions for geolocation.
    - Requires a new ENV variable: IPSTACK_ACCESS_KEY

---
+++ src/actions/geolocation.ts
@@ -0,0 +1,36 @@
+const axios = require("axios");
+const IPSTACK_ACCESS_KEY = process.env.IPSTACK_ACCESS_KEY;
+
+export async function getLocationByIP () {
+  try {
+    const response = await axios(`http://api.ipstack.com/check?access_key=${IPSTACK_ACCESS_KEY}`);
+    return {
+      city: response.city,
+      state: response.region_code,
+      lat: response.latitude,
+      lng: response.longitude,
+    };
+  } catch (error: any) {
+    return error.response;
+  }
+}
+
+export async function getCurrentLocation () {
+  try {
+    const location = await getLocation();
+    return {
+      city: '',
+      state: '',
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    };
+  } catch(error: any) {
+    return {
+      response: 'There was an error getting your location.',
+    }
+  }
+}
+
+function getLocation (options?: PositionOptions) {
+  return new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
+}

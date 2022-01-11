8c14ba910c97e84da6323a89171a85b50770d5ad
clayrayy <74256588+clayrayy@users.noreply.github.com>
2022-01-11 16:30:48 -0500

    fix: add IPStack api url to workflows yml

--- src/helpers/destinations.ts
+++ src/helpers/destinations.ts
@@ -17,71 +17,135 @@ import Vegas from '@/public/assets/images/png/destinations/vegas.png';
 interface Destination {
   label: string;
   imgSrc: StaticImageData;
+  coords: {
+    lat: number;
+    lon: number;
+  };
 }
 
 export const destinations: Destination[] = [
   {
     label: 'Los Angeles',
     imgSrc: La,
+    coords: {
+      lat: 34.05223,
+      lon: -118.24368,
+    },
   },
   {
     label: 'Denver',
     imgSrc: Denver,
+    coords: {
+      lat: 39.7392,
+      lon: -104.9903,
+    },
   },
   {
     label: 'Vancouver',
     imgSrc: Vancouver,
+    coords: {
+      lat: 49.2827,
+      lon: -123.1207,
+    },
   },
   {
     label: 'Amsterdam',
     imgSrc: Amsterdamn,
+    coords: {
+      lat: 52.377956,
+      lon: 4.89707,
+    },
   },
   {
     label: 'Barcelona',
     imgSrc: Barcelona,
+    coords: {
+      lat: 41.390205,
+      lon: 2.154007,
+    },
   },
   {
     label: 'Chicago',
     imgSrc: Chicago,
+    coords: {
+      lat: 41.881832,
+      lon: -87.623177,
+    },
   },
 
   {
     label: 'Ibiza',
     imgSrc: Ibiza,
+    coords: {
+      lat: 38.906986,
+      lon: 1.421416,
+    },
   },
   {
     label: 'Kingston',
     imgSrc: Kingston,
+    coords: {
+      lat: 17.995147,
+      lon: -76.7846006,
+    },
   },
 
   {
     label: 'Montevideo',
     imgSrc: Montevideo,
+    coords: {
+      lat: -34.901112,
+      lon: -56.164532,
+    },
   },
   {
     label: 'Nimbin',
     imgSrc: Nimbin,
+    coords: {
+      lat: -28.5958,
+      lon: 153.223,
+    },
   },
 
   {
     label: 'Portland',
     imgSrc: Portland,
+    coords: {
+      lat: 45.523064,
+      lon: -122.676483,
+    },
   },
   {
     label: 'San Francisco',
     imgSrc: SanFran,
+    coords: {
+      lat: 37.773972,
+      lon: -122.431297,
+    },
   },
   {
     label: 'Seattle',
     imgSrc: Seattle,
+    coords: {
+      lat: 47.608013,
+      lon: -122.335167,
+    },
   },
   {
     label: 'Toronto',
     imgSrc: Toronto,
+    coords: {
+      lat: 43.65107,
+      lon: -79.347015,
+    },
   },
 
   {
     label: 'Vegas',
     imgSrc: Vegas,
+    coords: {
+      lat: 36.114647,
+      lon: -115.172813,
+    },
   },
 ];

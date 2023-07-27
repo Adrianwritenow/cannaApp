47e5202381cd52b63791789b8567184a08b7fb96
Adrianwritenow <adrianwritenow@gmail.com>
2023-07-27 10:54:22 -0400

    first commit

---
+++ src/constants/avatar.constants.ts
@@ -0,0 +1,14 @@
+export const AVATAR_SMILE = 0.3;
+
+export enum ARMATURE {
+  MALE = 'ArmatureTargetMale',
+  FEMALE = 'ArmatureTargetFemale',
+}
+
+export enum SCENE {
+  HALFBODY = 'halfbody-portrait-v1',
+  FULLBODY = 'fullbody-portrait-v1',
+  HALFBODY_TRANSPARENT = 'halfbody-portrait-v1-transparent',
+  FULLBODY_TRANSPARENT = 'fullbody-portrait-v1-transparent',
+  FULLBODY_POSTURE_TRANSPARENT = 'fullbody-posture-v1-transparent',
+}
